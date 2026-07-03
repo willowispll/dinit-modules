05e94a2340f469e4eecec4fbef53ec556510a1fc
willowispll <gorelikgleb+github@gmail.com>
2026-07-03 23:08:25 +0300

    modules/services/dinit: init

---
+++ modules/services/dinit/default.nix
@@ -0,0 +1,21 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+{
+  options.dinit.enable = lib.mkEnableOption "dinit service manager";
+
+  config = lib.mkIf config.dinit.enable {
+    finit.services.dinit = {
+      description = "dinit service manager";
+      command = "${pkgs.dinit}/bin/dinit -s /etc/dinit.d";
+      runlevels = "S12345789";
+      log = true;
+      respawn = true;
+    };
+
+    environment.systemPackages = [ pkgs.dinit ];
+  };
+}
